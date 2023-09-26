e7a8b9ecf9bcc97d7bf39211a0e9ea73d9663702
Ely Dorado <2018dorado@gmail.com>
2023-09-26 13:10:06 -0500

    annotations

--- notes.py
+++ notes.py
@@ -17,3 +17,6 @@
 # Models are templates of what the data should have/look like
 # views are where we get the specific info needed
 # and req handler is where we outline what to do with the requests (get, psot, etc)
+
+#MODELS --> VIEWS --> REQ_HANDLER
+#Data exists in the SQLite tables
