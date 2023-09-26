2cb306d946fa0824683c2b791289364710e1742f
Ely Dorado <2018dorado@gmail.com>
2023-09-26 09:43:58 -0500

    notes

--- notes.py
+++ notes.py
@@ -1,3 +1,8 @@
 #request handler CH 1
 #Animal requests.py CH 2, 3, 4, 5, 6
 #There are the two modules to study for now .It's the intro 
+
+#command to start a virtual env. ensures libraries and everything aren't
+#in the whole OS, they're just local to this project:
+
+#pipenv shell
