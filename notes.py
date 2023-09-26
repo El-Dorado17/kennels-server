0b7ed36f0c403e194a738d7c239b8bf71b1297bb
Ely Dorado <2018dorado@gmail.com>
2023-09-26 13:04:12 -0500

    annotations

--- notes.py
+++ notes.py
@@ -1,8 +1,14 @@
 #request handler CH 1
 #Animal requests.py CH 2, 3, 4, 5, 6
-#There are the two modules to study for now .It's the intro 
+#There are the two modules to study for now .It's the intro
 
 #command to start a virtual env. ensures libraries and everything aren't
 #in the whole OS, they're just local to this project:
 
 #pipenv shell
+
+#Bottom of animal request module shows some hard-coded animals,
+#but the actual goal is to have them in the SQLite table
+
+#used sqlite open database to open and view the sql table but I can't remember
+#how to do it from, scratch
